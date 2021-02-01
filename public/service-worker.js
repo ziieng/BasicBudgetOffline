2277d207ec3108a5ca4132e05404bf317a59fb5b
ziieng <cjengelhardt@gmail.com>
2021-02-01 14:52:08 -0800

    connect IndexedDB to page

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,6 +2,7 @@ const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
   "/",
+  "/api/transaction",
   "/index.html",
   "/index.js",
   "/db.js",
